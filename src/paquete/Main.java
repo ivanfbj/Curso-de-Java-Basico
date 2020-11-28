4a7efe683bcb1bd9ea9a937491e1ae4c97983fa2
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2020-11-27 21:39:44 -0500

    Leer desde el teclado

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -1,9 +1,33 @@
 package paquete;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+//java.io.*; Libreria que se utiliza para entrada y salida de flujos de información de datos, se captura datos del teclado.
+
+public class Main {
+	/*
+	 * Leer desde el teclado concat() toUpperCase() toLowerCase() lenght() Convertir variables numericas a String y viceversa
+	 */
 	public static void main(String[] args) {
 
-		System.out.println("Hola Mundo");
+		String texto = "";
+
+		InputStreamReader in = new InputStreamReader(System.in);// Se abre un flujo o Canal de datos que se reciben desde el exterior
+		BufferedReader buffer = new BufferedReader(in);// Espacio de memoria que almacena datos del canal
+
+		// System.out.println("prueba");
+
+		try {
+			System.out.println("Ingrese el texto que quiere almacenar en memoria:");
+
+			texto = buffer.readLine();// Este método lee el dato del teclado, lo captura y lo almacena en la variable
+
+		} catch (IOException e) {//Sé que el try catch me permite controlar errores, pero me queda la duda de como se utiliza correctamente.
+		}
+
+		System.out.println("Este es el texto que se almacenó en la variable: " + texto);
+
 	}
 }
