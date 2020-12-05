8f3075d6e0bbb5a9146f3c26b29833226ad8aa2e
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2020-12-04 21:26:04 -0500

    Escribir Textos en Java

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -1,33 +1,13 @@
 package paquete;
 
-import java.util.LinkedList;
-
 public class Main {
 
 	public static void main(String[] args) {
 
+		Textos t = new Textos();
 
-		LinkedList lista = new LinkedList();
-		
-		lista.add("Primera palabra");
-		lista.add(5);
-		lista.add(17);
-		lista.add("Ivan");
-		
-		System.out.println("El tamaño de la lista es: " + lista.size());
-		System.out.println(lista.get(0));
-		System.out.println(lista.get(1));
-		System.out.println(lista.get(2));
-		
-		
-		
-		System.out.println(lista.getFirst());
-		System.out.println(lista.getLast());
-		
-		lista.remove(0);
-		
-		System.out.println(lista.getFirst());
-		
+		t.escribir("archivo_prueba_escritura.txt");
+		//t.leer("archivo.txt");
+
 	}
 }
-  
--- src/paquete/Textos.java
+++ src/paquete/Textos.java
@@ -1,9 +1,35 @@
 package paquete;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.swing.JOptionPane;
 
 public class Textos {
+
+	public void escribir(String nombreArchivo) {
+		File f;// crea un objeto de tipo archivo
+		f = new File(nombreArchivo);// Se asigna un nombre al archivo
+
+		// escritura
+		try {
+			FileWriter w = new FileWriter(f);// FileWriter
+			BufferedWriter bw = new BufferedWriter(w);// BufferWriter
+			PrintWriter wr = new PrintWriter(bw);// PrintWriter
+
+			wr.write("Texto de prueba en la escritura desde JAVA\nProbando salto de linea en la escritura");
+			// wr.append(" - esto una concatenación al texto");
+			wr.close();
+			bw.close();// Se deben cerrar el flujo de datos de los objeto wr y bw, para que el archivo se guarde y cierre. y así visualizar la información
+
+		} catch (IOException e) {
+		}
+	}
 
 	public void leer(String nombreArchivo) {
 
@@ -22,6 +48,7 @@ public class Textos {
 					break;
 				}
 
+				// JOptionPane.showMessageDialog(null, temp);
 				System.out.println(temp);
 			}
 
