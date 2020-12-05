125999eb913b48eb4c1e7c695f4b46bdd1b0703c
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2020-12-04 19:16:28 -0500

    Listas Enlazadas con LinkedList en Java

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -1,10 +1,33 @@
 package paquete;
 
+import java.util.LinkedList;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		Textos t = new Textos();
-		t.leer("archivo.txt");
+
+		LinkedList lista = new LinkedList();
+		
+		lista.add("Primera palabra");
+		lista.add(5);
+		lista.add(17);
+		lista.add("Ivan");
+		
+		System.out.println("El tamaño de la lista es: " + lista.size());
+		System.out.println(lista.get(0));
+		System.out.println(lista.get(1));
+		System.out.println(lista.get(2));
+		
+		
+		
+		System.out.println(lista.getFirst());
+		System.out.println(lista.getLast());
+		
+		lista.remove(0);
+		
+		System.out.println(lista.getFirst());
+		
 	}
 }
+  
