478280d8da6f7cf50d8c9405a5ad8928548c4034
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2020-12-05 12:18:32 -0500

    Tutorial de Pilas (Stacks) en Java

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -1,13 +1,31 @@
 package paquete;
 
+import java.util.Stack;
+
 public class Main {
 
+
+	/*
+	 * push: insertar un dato en la pila
+	 * pop: retirar el ultimo dato ingresado
+	 * peek: para ver el ultimo dato de la pila
+	 * empty: para saber si hay o no datos en la pila
+	 * */
+	
+	//Pial = FILO fisrt-in , last-out
+	
 	public static void main(String[] args) {
 
-		Textos t = new Textos();
+		Stack pila = new Stack();
+		pila.push(50); // indice de 0
+		pila.push("Ivan"); // indice 1
+		pila.push(27);
+		pila.push("fernando");
+		System.out.println("El ultimo elemento en la pila es: " + pila.peek());
 
-		t.escribir("archivo_prueba_escritura.txt");
-		//t.leer("archivo.txt");
+		while (pila.empty() == false) {
+			System.out.println(pila.pop());
+		}
 
 	}
 }
