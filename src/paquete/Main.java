49f96849bf0da68ab07f2e8af492f5c6db6cd81a
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2020-12-05 19:31:56 -0500

    Ordenamiento por Inserción (Insertion_Sort) en Java
    Debo validar y entender bien está lógica, está chevére pero se debe entender bien

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -4,17 +4,11 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		int arreglo[]= {5,3,4,2,1,9,8,7,21,254,78,56,32,14,58,78,9541,6,10,11,12,13};
-		/*
-		 * 5,3,4,2
-		 * 3,4,2,5
-		 * 3,2,4,5
-		 * 2,3,4,5
-		 * */
+		int arreglo[]= {5,15,7,11,4,12,23,3,2,1,45,13,64,51,31,29,6};
+		/**/
 		
 		Ordenar o = new Ordenar();
-		o.ordenarBurbuja(arreglo);
-		
+		o.ordenarInsercion(arreglo);
 		
 		for (int i = 0; i < arreglo.length; i++) {
 			System.out.println(arreglo[i]);
--- src/paquete/Ordenar.java
+++ src/paquete/Ordenar.java
@@ -2,6 +2,23 @@ package paquete;
 
 public class Ordenar {
 
+	public void ordenarInsercion(int array[]) {
+
+		int aux, cont1, cont2;
+
+		for (cont1 = 1; cont1 < array.length; cont1++) {
+
+			aux = array[cont1];
+			
+			for (cont2 = cont1 - 1; cont2 >= 0 && array[cont2] > aux; cont2--) {
+				array[cont2 + 1] = array[cont2];
+				array[cont2] = aux;
+			}
+
+		}
+
+	}
+
 	public void ordenarBurbuja(int array[]) {
 
 		int aux;
@@ -20,7 +37,7 @@ public class Ordenar {
 					cambios = true;
 				}
 			}
-			if(cambios==false) {
+			if (cambios == false) {
 				break;
 			}
 		}
