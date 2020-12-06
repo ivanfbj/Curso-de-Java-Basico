dca93acf83d81e2f68b654bb1575df63bb72855c
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2020-12-05 22:30:57 -0500

    Ordenamiento Quicksort(Rápido) en Java
    Debo validar y entender bien está lógica, está chevére pero se debe entender bien

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -1,18 +1,18 @@
 package paquete;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
-		int arreglo[]= {5,15,7,11,4,12,23,3,2,1,45,13,64,51,31,29,6};
+
+		int arreglo[] = { 5, 11, 7, 15, 4, 12, 4, 75, 1, 3, 5, 7, 6, 23, 3, 4, 2, 1, 45, 13 };
 		/**/
-		
+
 		Ordenar o = new Ordenar();
-		o.ordenarInsercion(arreglo);
-		
+		o.ordenarQuickSort(arreglo);
+
 		for (int i = 0; i < arreglo.length; i++) {
 			System.out.println(arreglo[i]);
 		}
-		
+
 	}
 }
--- src/paquete/Ordenar.java
+++ src/paquete/Ordenar.java
@@ -2,6 +2,46 @@ package paquete;
 
 public class Ordenar {
 
+	public void ordenarQuickSort(int array[]) {
+		array = quicksort1(array);
+	}
+
+	public int[] quicksort1(int numeros[]) {
+		return quicksort2(numeros, 0, numeros.length - 1);
+	}
+
+	public int[] quicksort2(int numeros[], int izq, int der) {
+		if (izq > der) {
+			return numeros;
+		}
+		int i = izq, d = der;
+		if (izq != der) {
+			int pivote;
+			int aux;
+			pivote = izq;
+			while (izq != der) {
+				while (numeros[der] >= numeros[pivote] && izq < der) {
+					der--;
+				}
+				while (numeros[izq] < numeros[pivote] && izq < der) {
+					izq++;
+				}
+				if (der != izq) {
+					aux = numeros[der];
+					numeros[der] = numeros[izq];
+					numeros[izq] = aux;
+				}
+			}
+			if (izq == der) {
+				quicksort2(numeros, i, izq - 1);
+				quicksort2(numeros, izq + 1, d);
+			}
+		} else {
+			return numeros;
+		}
+		return numeros;
+	}
+
 	public void ordenarInsercion(int array[]) {
 
 		int aux, cont1, cont2;
@@ -9,14 +49,12 @@ public class Ordenar {
 		for (cont1 = 1; cont1 < array.length; cont1++) {
 
 			aux = array[cont1];
-			
+
 			for (cont2 = cont1 - 1; cont2 >= 0 && array[cont2] > aux; cont2--) {
 				array[cont2 + 1] = array[cont2];
 				array[cont2] = aux;
 			}
-
 		}
-
 	}
 
 	public void ordenarBurbuja(int array[]) {
