5cbd5f05e57b674fe9b0a2c9a9e4a81b6a573a44
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2020-12-30 20:17:47 -0500

    Se realiza una mejora de la clase conexión
    - Realizar el cargue del controlador una unica vez en la ejecución de la aplicación, para no tener que estar realizando el cargue del controlador.

--- src/pruebaConexionBaseDeDatos/ConexionSql.java
+++ src/pruebaConexionBaseDeDatos/ConexionSql.java
@@ -12,6 +12,21 @@ public class ConexionSql {
 	private static final String USER_DB = "root";
 	private static final String PASS_DB = "1037626026";
 
+	
+	//Para evitar cargar el controlador cada vez que se intancie el objeto y se llame al metodo "conectar"
+	//Deberemos colocar el Class.form que se encarga de llamar al controlador, dentro de un bloque STATIC
+	//Esto es debido a que no necesitamos estar cargando el controlador cada vez que llamemos al método "conectar"-
+	//Esta carga del controlador lo podemos hacer la primera vez que ejecutemos toda la aplicación, cuando la aplicación independiente que llamemos o no el método "conectar"
+	
+	static {
+		try {
+			Class.forName(CONTROLADOR);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error al cargar el controlador");
+			e.printStackTrace();// Comando opcional
+		}
+	}
+	
 	// Con este metodo se vuelva la clase más reutilizable
 	public Connection conectar() {
 
@@ -19,17 +34,22 @@ public class ConexionSql {
 
 		try {
 			// Class.forName("com.mysql.jdbc.Driver");
-			Class.forName(CONTROLADOR);//Se carga el controlador que me permite conectarme a la base de datos de MySql
+			
+			/*Se mueve este gragmento del código al bloque Static*/
+			//Class.forName(CONTROLADOR);//Se carga el controlador que me permite conectarme a la base de datos de MySql
 
 			conexion = DriverManager.getConnection(URL_DB, USER_DB, PASS_DB);//Se establece la conexión con la base de datos, usuario y clave
 
 			System.out.println("Conexión OK");
 
-		} catch (ClassNotFoundException e) {
-			System.out.println("Error al cargar el controlador");
-			e.printStackTrace();// Comando opcional
-
-		} catch (SQLException e) {
+		}
+//		catch (ClassNotFoundException e) {
+//			//Estas 2 linea se mueven al ClassNotFoundException del bloque STATIC
+//			System.out.println("Error al cargar el controlador");
+//			e.printStackTrace();// Comando opcional
+//
+//		} 
+		catch (SQLException e) {
 			System.out.println("Error en la conexión SQL");
 			e.printStackTrace();// Comando opcional
 		}
